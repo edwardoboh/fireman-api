aeb61fdbf2c56804fb1c9b67e49d75d152b3e51c
edwardoboh <obohedward@gmail.com>
2020-12-28 19:17:26 -0500

    correction in server file

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true , useUnifiedTopo
 })
 
 if(process.env.NODE_ENV === "production"){
-    app.use("/home", express.static("client/public"))
+    app.use(express.static("client/build"))
 
     app.get("*", (request, response) => {
         response.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
